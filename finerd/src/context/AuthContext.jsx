22d7d9b07aca4f559a909d1c4055b00aec8b66d7
Larryson Chijioke <larrysongift@gmail.com>
2025-11-22 17:26:26 -0800

    Fix Netlify config + clean install

--- finerd/src/context/AuthContext.jsx
+++ finerd/src/context/AuthContext.jsx
@@ -65,7 +65,7 @@ export function AuthProvider(props) {
                 setIsLoading(false)
             }
         })
-        return unsubscribe
+        return() => unsubscribe ()
     }, [])
 
     return (
